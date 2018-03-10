c672d9ebf847ec886fe34c8ba73b512d7909e99c
Florian <me@florian.ws>
2018-03-10 02:54:40 +0100

    Initial commit

---
+++ jobs.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"time"
+)
+
+//job : lastExecutionTime
+var jobs = make([]*job, 0)
+
+type job struct {
+	fun func() //Function to execute
+
+	period int64 //Every period seconds
+
+	lastExec int64 //Last time it got executed
+	initExec bool //Execute when job is created
+}
+
+//The function you want to execute
+func (j *job) Func(f func()) *job {
+	j.fun = f
+	return j
+}
+
+//The period you want to execute the function in seconds (e.g. every 5 seconds)
+func (j *job) Period(period int64) *job {
+	j.period = period
+	return j
+}
+
+//Shall it execute the function at Start()?
+func (j *job) InitExec() *job {
+	j.initExec = true
+	return j
+}
+
+func (j *job) Start() {
+	if j.fun == nil {
+		panic("Func is nil")
+	}
+
+	if j.period <= 0 {
+		panic("Period is <= 0")
+	}
+
+	if j.lastExec == 0 {
+		j.lastExec = time.Now().Unix()
+	}
+	jobs = append(jobs, j)
+
+	if j.initExec {
+		Runner(j.fun)
+	}
+}
+
+func Create() *job {
+	return &job{}
+}
---
+++ scheduler.go
@@ -0,0 +1,39 @@
+package main
+
+import "time"
+
+//You can add a custom runner if you want so. It is used to executed a job's method
+var Runner = func(f func()) { f() }
+
+func schedulerFunc() {
+	for _, job := range jobs {
+		println("Checking job")
+		if job.lastExec > time.Now().Unix() { //Not ready yet.
+			continue
+		}
+
+		checkPeriodic(job)
+
+		time.Sleep(time.Second * 1) //1 is the minimum schedule time
+	}
+}
+
+func checkPeriodic(job *job) {
+	nowSec := time.Now().Unix()
+
+	if job.lastExec + job.period > nowSec {
+		return
+	}
+
+	Runner(job.fun)
+
+	job.lastExec = nowSec
+}
+
+func StartScheduler() {
+	go func() {
+		for {
+			schedulerFunc()
+		}
+	}()
+}
